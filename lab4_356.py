456f61ab601ea0f068d26ea35ed346ac9b2568b6
Stakhmich Karina <karinastahmic@gmail.com>
2025-11-01 14:53:39 +0200

    add

--- lab4_356.py
+++ lab4_356.py
@@ -1,13 +1,13 @@
-a = int(input("Enter the start number: "))
 n = int(input("Enter the end number: "))
 
 total = 0
 expression = ""
 
-for i in range(a, n):
+for i in range(1, n):
     term = f"{i}*{i+1}"
     expression += term
     if i != n - 1:
         expression += "+"
     total += i * (i + 1)
+
 print(expression + "=" + str(total))
